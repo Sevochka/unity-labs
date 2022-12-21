8f65e616635be9c31d2bc683a46a58ac961ceacf
Vsevolod Kochnev <v.kochnev@sports.ru>
2022-12-21 16:43:15 +0300

    add menu

--- Lab 4/Assets/scripts/CameraController.cs
+++ Lab 4/Assets/scripts/CameraController.cs
@@ -7,23 +7,47 @@ public class CameraController : MonoBehaviour
     [SerializeField] private float mouseSensetivity;
 
     private Transform parent;
+
+    public GameObject canvas;
     // Start is called before the first frame update
     private void Start()
     {
         parent = transform.parent;
-        Cursor.lockState = CursorLockMode.Locked;
+        canvas = GameObject.FindGameObjectWithTag("Canvas");
+        ReturnToGame();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            canvas.gameObject.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+        }
         Rotate();
     }
 
     private void Rotate()
     {
+        if (Cursor.lockState == CursorLockMode.None)
+        {
+            return;
+        }
+        
         float mouseX = Input.GetAxis("Mouse X") * mouseSensetivity * Time.deltaTime;
         
-        parent.Rotate(Vector3.up, mouseX);
+        parent.Rotate(Vector3.up, mouseX);   
+    }
+
+    public void ReturnToGame()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        canvas.gameObject.SetActive(false);
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
